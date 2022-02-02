57351c7013f760c86e30b2752bd094468c41d0bd
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-02-02 18:02:28 +0000

    thanks Fireship

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,25 +1,25 @@
 <script>
-  import { username, user } from './user';
+	import { username, user } from "./user";
 
-  function signout() {
-    user.leave();
-    username.set('');
-  }
+	function signout() {
+		user.leave();
+		username.set('');
+	}
 </script>
 
 <header>
-<h1>🔫💬</h1>
-  {#if $username}
-    <div class="user-bio">
-      
-      <span>Hello <strong>{$username}</strong></span>
-      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
-    </div>
+<h1>eChat</h1>
+	{#if $username}
+		<div class="user-bio">
+			
+			<span>Hello <strong>{$username}</strong></span>
+			<img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+		</div>
 
-    <button class="signout-button" on:click={signout}>Sign Out</button>
+		<button class="signout-button" on:click={signout}>Sign Out</button> 
 
-    {:else}
+		{:else}
 
-      <h3>Gun.js Chat</h3>
-  {/if}
+			<h3>Gun.js Chat</h3>
+	{/if}
 </header>
