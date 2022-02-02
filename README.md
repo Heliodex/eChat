0e9cdcd76657a5ae3aa9e75f247b6cc79df4369f
Lewin Kelly <heli@odex.cf>
2022-02-02 18:02:39 +0000

    thanks Fireship

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-# GUN Chat Dapp Demo
-
-A decentralized chat app built with [GUN](https://gun.eco/). 
-
-- Try the [Live Demo](https://gun-chat-dapp.web.app/)
-- Watch the [Chat Dapp Video](https://youtu.be/J5x3OMXjgMc)
-
-```
-git clone <this-repo>
-npm install
-npm run dev
-```
+test
--- src/user.js
+++ src/user.js
@@ -1,7 +1,7 @@
-import GUN from 'gun';
-import 'gun/sea';
-import 'gun/axe';
-import { writable } from 'svelte/store';
+import GUN from "gun";
+import "gun/sea";
+import "gun/axe";
+import { writable } from "svelte/store";
 
 // Database
 export const db = GUN();
@@ -9,13 +9,13 @@ export const db = GUN();
 // Gun User
 export const user = db.user().recall({sessionStorage: true});
 
-// Current User's username
-export const username = writable('');
+// Current User"s username
+export const username = writable("");
 
-user.get('alias').on(v => username.set(v))
+user.get("alias").on(v => username.set(v))
 
-db.on('auth', async(event) => {
-    const alias = await user.get('alias'); // username string
+db.on("auth", async(event) => {
+    const alias = await user.get("alias"); // username string
     username.set(alias);
 
     console.log(`signed in as ${alias}`);
