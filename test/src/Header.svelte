49b3e4b33f22d44a849838b953cc4db1054b6490
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-05-03 18:42:53 +0100

    Why has header.svelte reverted?!??!

---
+++ test/src/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { user } from "./user"
+
+	function signout() {
+		user.leave()
+		username.set("")
+	}
+</script>
+
+<header>
+	<h1>eChat</h1>
+	{#if $username}
+		<div class="user-bio">
+			<!-- <img  class="avatar" src={`https://avatars.dicebear.com/api/identicon/eChat_${$username}.svg?rotate=45`} alt="Your avatar" /> -->
+			<span><strong>{$username}</strong></span>
+		</div>
+
+		<button class="signout-button" on:click={signout}>Sign Out</button>
+	{:else}
+		<h3>Gun.js Chat</h3>
+	{/if}
+</header>
