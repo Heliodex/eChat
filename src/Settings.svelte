fc55383a0d678d0bfdadd91b7842a6c0b38c80b1
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-05-10 00:21:07 +0100

    Work on simple settings menu (no functionality yet)

--- src/Settings.svelte
+++ src/Settings.svelte
@@ -1,7 +1,26 @@
 <script>
+	let darkMode = false
+	let historyLength = 100
+	let theme = "green"
 </script>
 
 <div class="scrollable">
 	<h3>Settings</h3>
-	<p class="agreement">Hello worl!</p>
+	<div class="agreement settingsPage">
+		<div>Dark mode</div>
+		<input bind:value={darkMode} type="checkbox" />
+
+		<div>Message history length</div>
+		<label>
+			<input class="settingsInput" bind:value={historyLength} type="range" min="0" max="100" />
+			<div class="settingsLabel">{historyLength}</div>
+		</label>
+
+		<div>Theme</div>
+		<select bind:value={theme}>
+			<option value="green">Sea green</option>
+			<option value="blue">Bubble blue</option>
+			<option value="orange">Orange orange</option>
+		</select>
+	</div>
 </div>
