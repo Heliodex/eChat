0252299b5fca3f64e13ed1bd17b579c746dcc9d4
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-05-15 09:17:35 +0100

    Format

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
-// import liveReload from "vite-plugin-live-reload"
-// import FullReload from 'vite-plugin-full-reload'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		svelte(),
-		// liveReload("src")
-		// FullReload("src", { always: true })
-	],
+	plugins: [svelte()],
 	server: {
 		watch: {
 			usePolling: true
