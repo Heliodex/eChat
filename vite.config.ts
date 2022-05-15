dba29910d2d372a6fc1ca8538be076c947ea1e58
Lewin Kelly <heli@odex.cf>
2022-05-15 06:04:41 +0100

    HMR WORKS NOW: youtu.be/BUClW9wTqGQ THANK YOU SO MUCH

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+// import liveReload from "vite-plugin-live-reload"
+// import FullReload from 'vite-plugin-full-reload'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+	plugins: [
+		svelte(),
+		// liveReload("src")
+		// FullReload("src", { always: true })
+	],
+	server: {
+		watch: {
+			usePolling: true
+		}
+	}
 })
