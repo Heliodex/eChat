84d56e889cd53b037e4d636fa45978fd3cd0b6bf
Lewin Kelly <heli@odex.cf>
2022-05-16 01:36:59 +0100

    New token, old one expired. Need to make this more stable

--- src/user.ts
+++ src/user.ts
@@ -2,11 +2,9 @@ import Centrifuge from "centrifuge"
 import { writable } from "svelte/store"
 
 export const centrifuge = new Centrifuge("ws://echat.ddns.net:8000/connection/websocket")
-centrifuge.setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM3MjIiLCJleHAiOjE2NTI2MDQ3MzJ9.KGx3O9kS4K-z8Ydah-_qf892mdWy0PsLqZQEIanAnYs")
+centrifuge.setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM3MjIiLCJleHAiOjE2NTMyNjUyODh9.9dMsEPH65UBS382_-M_7bBUEPniPThqPILlxoA6TWA4")
 
 export const loginInfo = writable({
 	groupname: "",
 	username: localStorage.getItem("username"),
 })
-
-
