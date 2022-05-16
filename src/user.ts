77a5fb2e1a076c6ee2566eaa2e13e78f200ca60a
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-05-16 02:32:19 +0100

    New token: this one will not expire for 10 years

--- src/user.ts
+++ src/user.ts
@@ -2,7 +2,7 @@ import Centrifuge from "centrifuge"
 import { writable } from "svelte/store"
 
 export const centrifuge = new Centrifuge("ws://echat.ddns.net:8000/connection/websocket")
-centrifuge.setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM3MjIiLCJleHAiOjE2NTMyNjUyODh9.9dMsEPH65UBS382_-M_7bBUEPniPThqPILlxoA6TWA4")
+centrifuge.setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM3MjIiLCJleHAiOjE5NTI2NjQyMzN9.G2qQL1Vsr9jv1_UDM2dnoTIVjyxK2m6yu7rgMtSGnhQ")
 
 export const loginInfo = writable({
 	groupname: "",
