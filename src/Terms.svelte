ec897f52f446f5d15770b62f10de4450a9e8ce4f
Lewin Kelly <heli@odex.cf>
2022-05-22 11:42:13 +0100

    Improve Terms of Service with list elements

--- src/Terms.svelte
+++ src/Terms.svelte
@@ -13,42 +13,49 @@
 		<br />
 		eChat hereby grants you a non-transferable, non-exclusive license to use eChat or any of their services, provided that you agree to, comply with, and accept the entirety of the following:<br />
 		<br />
-		1. You enter and participate in chat groups and obtain access to the materials contained at your own risk.<br />
+	</p>
+
+	<ol>
+		<li>You enter and participate in chat groups and obtain access to the materials contained at your own risk.</li>
 		<br />
-		2. You are 13 years of age or older, or have obtained parental consent to use eChat.<br />
+		<li>You are 13 years of age or older, or have obtained parental consent to use eChat.</li>
 		<br />
-		3. You are fully responsible for your own statements and materials that you post to eChat and any consequences. You agree that you will not take any action directed towards attempting to hold us responsible for any such materials or statements.<br />
+		<li>You are fully responsible for your own statements and materials that you post to eChat and any consequences. You agree that you will not take any action directed towards attempting to hold us responsible for any such materials or statements.</li>
 		<br />
-		4. Any opinions or views expressed by participants on eChat are their own. We do not endorse, support, or otherwise give any credence or reason for reliance on any such statements or opinions.<br />
+		<li>Any opinions or views expressed by participants on eChat are their own. We do not endorse, support, or otherwise give any credence or reason for reliance on any such statements or opinions.</li>
 		<br />
-		5. eChat is not required to monitor communications on chat groups, and neither we nor any Administrators are responsible for any material that any participants post, but we may do so at our sole discretion.<br />
+		<li>eChat is not required to monitor communications on chat groups, and neither we nor any Administrators are responsible for any material that any participants post, but we may do so at our sole discretion.</li>
 		<br />
-		6. We reserve the right to remove or expel any party from participation and access to eChat for any time or for any reason.<br />
+		<li>We reserve the right to remove or expel any party from participation and access to eChat for any time or for any reason.</li>
 		<br />
-		7. By accessing eChat, you agree that you will not commit any of the following acts:<br />
+		<li>By accessing eChat, you agree that you will not commit any of the following acts:</li>
 		<br />
-		7.1. Using eChat for any illegal purpose under applicable law.<br />
+		<ol>
+			<li>Using eChat for any illegal purpose under applicable law.</li>
+			<br />
+			<li>Placing any material in eChat that you do not have a right to upload, link to, post, or otherwise transmit under applicable law.</li>
+			<br />
+			<li>Placing any material in eChat that contains a false statement about any person/legal entity, infringes upon the privacy rights of any other person/legal entity, or threatens, harrases, abuses, defames or embarrasses any other person/legal entity without explicit permission from them.</li>
+			<br />
+			<li>Placing any obscene, pornographic, sexually explicit or violent materials in eChat.</li>
+			<br />
+			<li>Repeatedly placing any unsolicited advertising, marketing materials, or sales/promotional materials in eChat.</li>
+			<br />
+			<li>Placing any material in the eChat that is disruptive or defamatory to eChat or other users of the eChat service including Administrators.</li>
+		</ol>
 		<br />
-		7.2. Placing any material in eChat that you do not have a right to upload, link to, post, or otherwise transmit under applicable law.<br />
+		<li>By placing any material in eChat, you hereby grant any viewers of the material a perpetual, non-revocable, royalty-free license to view, download, store, and reproduce such material.</li>
 		<br />
-		7.3. Placing any material in eChat that contains a false statement about any person/legal entity, infringes upon the privacy rights of any other person/legal entity, or threatens, harrases, abuses, defames or embarrasses any other person/legal entity without explicit permission from them.<br />
+		<li>Any Users or Administrators of eChat reserve the right to stay anonymous and unnamed if they wish to.</li>
 		<br />
-		7.4. Placing any obscene, pornographic, sexually explicit or violent materials in eChat.<br />
+		<li>You may use eChat or any of their services only as outlined in this agreement. Any use of eChat or any of their services that is not expressly allowed in this Agreement will automatically terminate your rights to use eChat under this agreement. If your right to use eChat or any of their services is terminated, you shall immediately stop using eChat.</li>
 		<br />
-		7.5. Repeatedly placing any unsolicited advertising, marketing materials, or sales/promotional materials in eChat.<br />
+		<li>This agreement contains the entire agreement between the Parties. No variation of any of the terms or conditions of this agreement may be made unless such variation is explicitly agreed in writing and signed by both you and eChat.</li>
+	</ol>
+
+	<p class="agreement">
 		<br />
-		7.6. Placing any material in the eChat that is disruptive or defamatory to eChat or other users of the eChat service including Administrators.<br />
-		<br />
-		8. By placing any material in eChat, you hereby grant any viewers of the material a perpetual, non-revocable, royalty-free license to view, download, store, and reproduce such material.<br />
-		<br />
-		9. Any Users or Administrators of eChat reserve the right to stay anonymous and unnamed if they wish to.<br />
-		<br />
-		10. You may use eChat or any of their services only as outlined in this agreement. Any use of eChat or any of their services that is not expressly allowed in this Agreement will automatically terminate your rights to use eChat under this agreement. If your right to use eChat or any of their services is terminated, you shall immediately stop using eChat.<br />
-		<br />
-		11. This agreement contains the entire agreement between the Parties. No variation of any of the terms or conditions of this agreement may be made unless such variation is explicitly agreed in writing and signed by both you and eChat.<br />
-		<br />
-		<br />
-		This agreement is governed by and constructed in accordance with the applicable laws of Scotland, England, Northern Ireland, and Wales. The https://echatapp.ml/ and https://echat.cf/ domains are solely governed by and constructed in accordance with the laws of the Central African Republic, Mali, or any entities connected with them, including connected corporations/companies such as SEDO, Société Centrafricaine de Télécommunications (SOCATEL), Point.ml, Dot.cf, or United Internet AG.<br />
+		This agreement is governed by and constructed in accordance with the applicable laws of Scotland, England, Northern Ireland, and Wales. The domains https://echatapp.ml/ and https://echat.cf/ are solely governed by and constructed in accordance with the laws of the Central African Republic, Mali, or any entities connected with them, including connected corporations/companies such as SEDO, Société Centrafricaine de Télécommunications (SOCATEL), Point.ml, Dot.cf, or United Internet AG.<br />
 		<br />
 		If any term of this agreement is adjudged to be invalid or unenforceable, such term shall in no way affect any other term of this agreement or the application of such term in any other circumstance or the validity or enforceability of this Agreement. The Parties will negotiate in good faith about a term that will replace the invalid or unenforceable term.<br />
 		<br />
