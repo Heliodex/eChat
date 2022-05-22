abd1e9d3c9f4290809e8cd5f20e60ac08d65b3be
Lewin Kelly <heli@odex.cf>
2022-05-22 13:33:28 +0100

    Add day to timestamp and align it correctly

--- src/ChatMessage.svelte
+++ src/ChatMessage.svelte
@@ -3,6 +3,7 @@
 	import { messageType } from "./types"
 	export let msg: messageType
 
+	const week = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
 	const timestamp: Date = new Date(msg.timestamp)
 
 	$: messageClass = msg.username == $loginInfo["username"]?.toString() ? "sent" : "received"
@@ -15,12 +16,12 @@
 		<p>
 			{msg.text}
 			<br />
-			<time class="time">
+			<i class="time">
 				{#if userMessage}
 					{msg.username.toString()},
 				{/if}
-				{timestamp.toTimeString().substring(0, 5)}
-			</time>
+				{week[timestamp.getDay()]} {timestamp.toTimeString().substring(0, 5)} <!-- or {timestamp.getHours()}:{timestamp.getMinutes()} -->
+			</i>
 			<!-- a much better solution than DocSocial -->
 		</p>
 	</div>
