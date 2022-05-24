855e174ac0c48cf00cfcf11a84545bc51951d1ea
Lewin Kelly <heli@odex.cf>
2022-05-25 00:46:06 +0100

    Bold font seems to not work properly on MacOS?

--- src/Help.svelte
+++ src/Help.svelte
@@ -1,84 +1,74 @@
 <div class="scrollable">
 	<p class="agreement">
-		<br>
-		<b id="b">How to go back</b>
-		<br>
-		To go back in eChat, click the back/exit icon in the top left corner.
-		<br>
-		<br>
-		<b class="b">How to create a message group</b>
-		<br>
-		From the homepage, enter your desired group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and your group should be created.
-		<br>
-		<br>
-		<b id="b">How to create a username</b>
-		<br>
-		From the homepage, enter your desired username into the username box. Before clicking the join button, make sure you’ve entered your/the group name.
-		<br>
-		<br>
-		<b id="b">How to change username</b>
-		<br>
-		To change your username, exit the message group that you’re in. Then enter a different username into the username box and rejoin the message group. Before clicking the join button, make sure you’ve entered your/the group name.
-		<br>
-		<br>
-		<b id="b">How to join a message group</b>
-		<br>
-		From the homepage, enter the group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and you should join the message group.
-		<br>
-		<br>
-		<b id="b">How to leave/exit a message group</b>
-		<br>
-		To leave a message group click the back/exit icon in the top left corner.
-		<br>
-		<br>
-		<b id="b">How to switch message groups</b>
-		<br>
-		Once in a message group, click the back/exit icon in the top left corner. Then enter a different group name into the group name box and join the message group. Before clicking the join button, make sure you’ve entered your username.
-		<br>
-		<br>
-		<b id="b">How to send messages</b>
-		<br>
-		Once in a message group, enter your chosen message in the message box at the bottom of the page. Once completed, either type the enter key or click the send button to the right of the message box. Please remember to make sure that any messages sent on eChat adhere to our Terms of Service.
-		<br>
-		<br>
-		<b id="b">How to unsend messages</b>
-		<br>
-		Currently eChat does not support the unsending of messages.
-		<br>
-		<br>
-		<b id="b">How to send images</b>
-		<br>
-		Currently eChat does not support the sending of messages containing images. You can alternatively send image sharing links on eChat. Please remember to make sure that any messages sent on eChat adhere to our Terms of Service.
-		<br>
-		<br>
-		<b id="b">How to switch to dark mode</b>
-		<br>
-		From the homepage, click the settings icon in the top right corner. In settings, select the dark mode checkbox and you should see that the theme of eChat changes from white to black. You have now joined the dark side. If you want to switch back then simply unselect the checkbox.
-		<br>
-		<br>
-		<b id="b">How to switch theme colour</b>
-		<br>
-		From the homepage, click the settings icon in the top right corner. In settings, select the theme dropdown. You can choose from “Sea Green”, “Bubble blue” and “Juicy Orange”. Once selected you should see that the theme of eChat changes colour. If you want to switch back then simply select your previous Theme colour. 
-		<br>
-		<br>
-		<b id="b">How to change history length</b>
-		<br>
-		From the homepage, click the settings icon in the top right corner. In settings, select and drag the message history length selector. You should see that the length moves up/down.
-		<br>
-		<br>
-		<b id="b">How to read the terms of service</b>
-		<br>
-		From the homepage, click the Terms of Service link at the bottom. You should then be able to read the Terms of Service.
-		<br>
-		<br>
-		<b id="b">How to report a user</b>
-		<br>
+		<br />
+		<b>How to go back</b><br />
+		How to create a message group<br />
+		From the homepage, enter your desired group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and your group should be created.<br />
+		<br />
+		<b>How to create a username</b><br />
+		From the homepage, enter your desired username into the username box. Before clicking the join button, make sure you've entered your/the group name.<br />
+		<br />
+		<b>How to create a message group</b><br />
+		How to join a message group<br />
+		From the homepage, enter the group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and you should join the message group.<br />
+		<br />
+		<b>How to leave/exit a message group</b><br />
+		To leave a message group click the back/exit icon in the top left corner.<br />
+		<br />
+		<b>How to create a username</b><br />
+		How to send messages<br />
+		Once in a message group, enter your chosen message in the message box at the bottom of the page. Once completed, either type the enter key or click the send button to the right of the message box. Please remember to make sure that any messages sent on eChat adhere to our Terms of Service.<br />
+		<br />
+		<b>How to unsend messages</b><br />
+		Currently, eChat does not support the unsending of messages.<br />
+		<br />
+		<b>How to change username</b><br />
+		To change your username, exit the message group that you're in. Then enter a different username into the username box and rejoin the message group. Before clicking the join button, make sure you've entered your/the group name.<br />
+		<br />
+		<b>How to join a message group</b><br />
+		From the homepage, enter the group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and you should join the message group.<br />
+		<br />
+		<b>How to leave/exit a message group</b><br />
+		To leave a message group click the back/exit icon in the top left corner.<br />
+		<br />
+		<b>How to switch message groups</b><br />
+		Once in a message group, click the back/exit icon in the top left corner. Then enter a different group name into the group name box and join the message group. Before clicking the join button, make sure you've entered your username.<br />
+		<br />
+		<b>How to send messages</b><br />
+		Once in a message group, enter your chosen message in the message box at the bottom of the page. Once completed, either type the enter key or click the send button to the right of the message box. Please remember to make sure that any messages sent on eChat adhere to our Terms of Service.<br />
+		<br />
+		<b>How to unsend messages</b><br />
+		Currently eChat does not support the unsending of messages.<br />
+		<br />
+		<b>How to send images</b><br />
+		Currently eChat does not support the sending of messages containing images. You can alternatively send image sharing links on eChat. Please remember to make sure that any messages sent on eChat adhere to our Terms of Service.<br />
+		<br />
+		<b>How to switch to dark mode</b><br />
+		From the homepage, click the settings icon in the top right corner. In settings, select the dark mode checkbox and you should see that the theme of eChat changes from white to black. You have now joined the dark side. If you want to switch back then simply unselect the checkbox.<br />
+		How to switch to dark mode<br />
+		From the homepage, click the settings icon in the top right corner. In settings, select the dark mode checkbox and you should see that the theme of eChat changes from white to black. You have now joined the dark side. If you want to switch back then simply unselect the checkbox.<br />
+		<br />
+		<b>How to switch theme colour</b><br />
+		From the homepage, click the settings icon in the top right corner. In settings, select the theme dropdown. You can choose from “Sea Green”, “Bubble blue” and “Juicy Orange”. Once selected you should see that the theme of eChat changes colour. If you want to switch back then simply select your previous Theme colour.
+		<br />
+		<br />
+		<b>How to change history length</b><br />
+		From the homepage, click the settings icon in the top right corner. In settings, select and drag the message history length selector. You should see that the length moves up/down.<br />
+		<br />
+		<b>How to read the terms of service</b><br />
+		From the homepage, click the Terms of Service link at the bottom. You should then be able to read the Terms of Service.<br />
+		<br />
+		<b>How to report a user</b><br />
 		To report a user please contact our support team
-		<br>
-		<br>
-		<b id="b">How to contact our support team</b>
-		<br>
-		To contact our support team please email: help.echat@gmail.com
-		<br>
-		<br>
+		<br />
+		<br />
+		<b>How to contact our support team</b><br />
+		<br />
+		<b>How to report a user</b><br />
+		To report a user please contact our support team<br />
+		<br />
+		How to contact our support team<br />
+		To contact our support team please email: help.echat@gmail.com<br />
+		<br />
+	</p>
 </div>
