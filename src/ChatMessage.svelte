f9515bf7a0877e6ee7c91475e1a91b885314fc36
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-05-30 02:47:58 +0100

    Extract global sass into component sass, less room for errors

--- src/ChatMessage.svelte
+++ src/ChatMessage.svelte
@@ -4,26 +4,55 @@
 	export let msg: messageType
 
 	const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-	const timestamp: Date = new Date(msg.timestamp)
+	const timestamp = new Date(msg.timestamp)
 
 	$: messageClass = msg.username == $loginInfo["username"]?.toString() ? "sent" : "received"
 	$: userMessage = msg.username != $loginInfo["username"]?.toString()
 </script>
 
 <div class={`message ${messageClass}`}>
-	<div class="messageText">
-		<p>
-			{msg.text}
-			<br />
-			<em class="time">
-				{#if userMessage}
-					{msg.username.toString()},
-				{/if}
-				{week[timestamp.getDay()]}
-				{timestamp.toTimeString().substring(0, 5)}
-				<!-- or {timestamp.getHours()}:{timestamp.getMinutes()} -->
-			</em>
-			<!-- a much better solution than DocSocial -->
-		</p>
-	</div>
+	<p>
+		{msg.text}
+		<br />
+		<em class="time">
+			{#if userMessage}
+				{msg.username.toString()},
+			{/if}
+			{week[timestamp.getDay()]}
+			{timestamp.getHours()}:{timestamp.getMinutes()}
+		</em>
+		<!-- a much better solution than DocSocial -->
+	</p>
 </div>
+
+<style lang="sass">
+	.sent
+		flex-direction: row-reverse
+		p
+			text-align: right
+		.time
+			float: right
+
+	.received p
+		text-align: left
+	
+	.time
+		display: flex
+		font-size: 0.5rem
+		line-height: 0.6rem
+
+	.sent p, .received p
+		max-width: 260px
+		margin: 4px 0.5rem 0 0.5rem
+
+		line-height: 1rem
+		font-size: 0.9rem
+
+		padding: 10px 15px 5px 15px
+		border-radius: 20px
+		position: relative
+		word-wrap: break-word
+
+	.message
+		display: flex
+</style>
