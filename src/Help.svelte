ec13b643780a006828b4f0f53c7416c38bebf441
Lewin Kelly <heli@odex.cf>
2022-06-08 10:52:56 +0100

    New help menu, looks more like a chat group

--- src/Help.svelte
+++ src/Help.svelte
@@ -3,52 +3,87 @@
 	import { transitionLength } from "./settings"
 </script>
 
-<div class="scrollable" transition:fly={{ y: 300, duration: 500 * parseFloat($transitionLength) }}>
-	<div class="agreement">
-		<br />
-		<h4>How to create a message group</h4>
-		<p>From the homepage, enter your desired group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and your group should be created.</p>
-		<h4>How to create a username</h4>
-		<p>From the homepage, enter your desired username into the username box. Before clicking the join button, make sure you've entered your/the group name.</p>
-		<h4>How to join a message group</h4>
-		<p>From the homepage, enter the group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and you should join the message group.</p>
-		<h4>How to leave/exit a message group</h4>
-		<p>To leave a message group click the back/exit icon in the top left corner.</p>
-		<h4>How to send messages</h4>
-		<p>Once in a message group, enter your chosen message in the message box at the bottom of the page. Once completed, either type the enter key or click the send button to the right of the message box. Please remember to make sure that any messages sent on eChat adhere to our Terms of Service.</p>
-		<h4>How to unsend messages</h4>
-		<p>Currently, eChat does not support the unsending of messages. If you wish for all messages in any room to be cleared, please contact our support team.</p>
-		<h4>How to change username</h4>
-		<p>To change your username, exit the message group that you're in. Then enter a different username into the username box and rejoin the message group. Before clicking the join button, make sure you've entered your/the group name.</p>
-		<h4>How to join a message group</h4>
-		<p>From the homepage, enter the group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and you should join the message group.</p>
-		<h4>How to leave/exit a message group</h4>
-		<p>To leave a message group click the back/exit icon in the top left corner.</p>
-		<h4>How to switch message groups</h4>
-		<p>Once in a message group, click the back/exit icon in the top left corner. Then enter a different group name into the group name box and join the message group. Before clicking the join button, make sure you've entered your username.</p>
-		<h4>How to send messages</h4>
-		<p>Once in a message group, enter your chosen message in the message box at the bottom of the page. Once completed, either type the enter key or click the send button to the right of the message box. Please remember to make sure that any messages sent on eChat adhere to our Terms of Service.</p>
-		<h4>How to unsend messages</h4>
-		<p>Currently eChat does not support the unsending of messages.</p>
-		<h4>How to send images</h4>
-		<p>Currently eChat does not support the sending of messages containing images. You can alternatively send image sharing links on eChat. Please remember to make sure that any messages sent on eChat adhere to our Terms of Service.</p>
-		<h4>How to switch to dark mode</h4>
-		<p>From the homepage, click the settings icon in the top right corner. In settings, select the dark mode checkbox and you should see that the theme of eChat changes from white to black. You have now joined the dark side. If you want to switch back then simply unselect the checkbox.</p>
-		<h4>How to switch theme colour</h4>
-		<p>From the homepage, click the settings icon in the top right corner. In settings, select the theme dropdown. You can choose from “Sea Green”, “Bubble blue” and “Juicy Orange”. Once selected you should see that the theme of eChat changes colour. If you want to switch back then simply select your previous Theme colour.</p>
-		<h4>How to change history length</h4>
-		<p>From the homepage, click the settings icon in the top right corner. In settings, select and drag the message history length selector. You should see that the length moves up/down.</p>
-		<h4>How to read the terms of service</h4>
-		<p>From the homepage, click the Terms of Service link at the bottom. You should then be able to read the Terms of Service.</p>
-		<h4>How to report a user</h4>
-		<p>To report a user please contact our support team with their details.</p>
-		<h4>How to contact our support team</h4>
-		<p>To contact our support team please email us at <a href="mailto:help.echat@gmail.com">help.echat@gmail.com</a>.</p>
-		<br />
-	</div>
+<div class="main" transition:fly={{ y: 300, duration: 500 * parseFloat($transitionLength) }}>
+	<div><p>How do I create a message group?</p></div>
+	<p>From the homepage, enter a group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and your group should be created.</p>
+
+	<div><p>How do I join a message group?</p></div>
+	<p>From the homepage, enter the group name into the group name box. Then enter your desired username into the username box. Once completed, click the join button below and you should join the message group.</p>
+
+	<div><p>How do I make a username?</p></div>
+	<p>From the homepage, enter a username into the username box. Before clicking the join button, make sure you've entered a group name as well.</p>
+
+	<div><p>How do I leave a message group?</p></div>
+	<p>To leave a message group, click the back button in the top left corner.</p>
+
+	<div><p>How do I send messages?</p></div>
+	<p>Once in a message group, enter your chosen message in the message box at the bottom of the page. Once completed, either press enter or click the send button to the right of the message box. Please remember to make sure that any messages sent on eChat adhere to our Terms of Service.</p>
+
+	<div><p>How do I delete messages?</p></div>
+	<p>Currently, eChat does not support the deleting of messages. If you wish for all messages in any room to be cleared, please contact our support team with the group name.</p>
+
+	<div><p>How do I change my username?</p></div>
+	<p>To change your username, exit the message group that you're in. Then enter a different username into the username box and rejoin the message group. Before clicking the join button, make sure you've entered a group name.</p>
+
+	<div><p>How do I send images?</p></div>
+	<p>Currently eChat does not support the sending of messages containing images. Alternatively, you can send image sharing links on eChat. Please remember to make sure that any images sent on eChat adhere to our Terms of Service.</p>
+
+	<div><p>How do I switch to dark mode?</p></div>
+	<p>From the homepage, click the settings icon in the top right corner. In settings, select the dark mode checkbox, and eChat's light colours will become dark. You have now joined the dark side.</p>
+
+	<div><p>How do I change theme colour?</p></div>
+	<p>From the homepage, click the settings icon in the top right corner. In settings, select the theme dropdown. Once you have selected theme, you should see that the accent colour across eChat changes.</p>
+
+	<div><p>How do I change history length?</p></div>
+	<p>From the homepage, click the settings icon in the top right corner. In settings, select and drag the message history length selector. This does not change history length for other users, only the number of messages loaded for you.</p>
+
+	<div><p>How do I read the terms of service?</p></div>
+	<p>From the homepage, click the Terms of Service link at the bottom. You should then be able to read the Terms of Service.</p>
+
+	<div><p>How do I report a user?</p></div>
+	<p>To report a user, please contact our support team with their details.</p>
+
+	<div><p>How do I contact eChat's support team?</p></div>
+	<p>To contact our support team, please email us at <a href="mailto:help.echat@gmail.com">help.echat@gmail.com</a>.</p>
+	<br />
 </div>
 
 <style lang="sass">
-	h4
-		line-height: 1.5rem
+	.main
+		position: absolute
+		display: flex
+		top: 10vh
+		height: 90vh
+		width: 100vw
+		overflow-x: hidden
+		overflow-y: enabled
+		flex-direction: column
+		div
+			display: flex
+			flex-direction: row-reverse
+			p
+				background: var(--t3)
+
+		p
+			background: var(--main)
+			text-align: left
+
+	div p, p
+			color: white
+			max-width: 260px
+			margin: 4px 0.5rem 0 0.5rem
+
+			line-height: 1rem
+			font-size: 0.9rem
+
+			padding: 10px 15px 5px 15px
+			border-radius: 20px
+			word-wrap: break-word
+
+	a
+		color: white
+		transition: color 0.2s
+		&:hover
+			transition: color 0.2s
+			color: #ddd
 </style>
