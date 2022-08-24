894144a01f244f476929d5f8233e6b8f021422bd
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-08-24 10:36:00 +0100

    Update vite config to work with https

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
+import mkcert from "vite-plugin-mkcert"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [svelte(), mkcert()],
 	server: {
+		https: true,
 		watch: {
 			usePolling: true
 		}
